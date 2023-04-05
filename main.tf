a89e95453b4027d86f6a356cbe4c3730857e595b
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-05 10:40:29 +0000

    job folder error

--- main.tf
+++ main.tf
@@ -5,9 +5,10 @@ resource "jenkins_folder" "folders" {
 
 resource "jenkins_job" "job" {
   count  = length(var.jobs)
-  name   = lookup(element(var.jobs, count.index), "name", null)
-  folder = lookup(element(var.jobs, count.index), "folder", null)
+  name   = "/job/${lookup(element(var.jobs, count.index), "name", null)}"
+  folder = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
   template = templatefile("${path.module}/sb-job.xml", {
     repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
   })
 }
+
