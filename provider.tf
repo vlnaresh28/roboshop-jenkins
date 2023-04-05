622782dbee332ba2e333a6dccc26273a4da67cbc
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-05 09:53:48 +0000

    jenkinip

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "jenkins" {
-  server_url = "http://172.31.6.243:8080"
+  server_url = "http://172.31.13.8:8080"
   username   = data.aws_ssm_parameter.jenkins_user.value
   password   = data.aws_ssm_parameter.jenkins_pass.value
 }
