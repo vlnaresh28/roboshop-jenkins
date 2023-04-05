ebe01dc61d42798e1d1f43dde0b5908e63c58cff
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-05 11:52:45 +0000

    vars

--- vars.tf
+++ vars.tf
@@ -1,9 +1,19 @@
 variable "folders" {
-  default = ["Infra"]
+  default = ["Infra", "CI-Pipeline"]
 }
 
 variable "jobs" {
   default = [
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/vlnaresh28/roboshop-infra" }
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/vlnaresh28/roboshop-infra1" },
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/vlnaresh28/frontend" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/vlnaresh28/cart" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/vlnaresh28/catalogue" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/vlnaresh28/user" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/vlnaresh28/shipping" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/vlnaresh28/payment" }
   ]
+}
+
+variable "force" {
+  default = false
 }
