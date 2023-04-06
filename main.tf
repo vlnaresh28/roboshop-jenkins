6eb4890f3a51b666b92126de2b34618d5b263764
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-06 15:41:50 +0000

    data,resourse

--- main.tf
+++ main.tf
@@ -18,4 +18,14 @@ resource "jenkins_job" "job" {
 
 }
 
+data "aws_instance" "jenkins" {
+  instance_id = "i-0c6467609af91bdea"
+}
 
+resource "aws_route53_record" "jenkins" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "jenkins.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [data.aws_instance.jenkins.public_ip]
+}
