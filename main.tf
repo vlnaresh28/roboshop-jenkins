bde578e8e4ca6b591f78b4d59fa874c82e7a41ae
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-11 14:12:36 +0000

    name

--- main.tf
+++ main.tf
@@ -3,49 +3,31 @@ resource "jenkins_folder" "folders" {
   name  = element(var.folders, count.index)
 }
 
-resource "jenkins_job" "s-job" {
+resource "jenkins_job" "job" {
   depends_on = [jenkins_folder.folders]
 
-  count  = length(var.s-jobs)
-  name   = lookup(element(var.s-jobs, count.index), "name", null)
-  folder = "/job/${lookup(element(var.s-jobs, count.index), "folder", null)}"
+  count  = length(var.jobs)
+  name   = lookup(element(var.jobs, count.index), "name", null)
+  folder = "/job/${lookup(element(var.jobs, count.index), "folder", null)}"
 
   template = templatefile("${path.module}/sb-job.xml", {
-    repo_url = lookup(element(var.s-jobs, count.index), "repo_url", null)
-    name     = lookup(element(var.s-jobs, count.index), "name", null)
+    repo_url = lookup(element(var.jobs, count.index), "repo_url", null)
+    name     = lookup(element(var.jobs, count.index), "name", null)
   })
 
-  lifecycle {
-    ignore_changes = [template]
-  }
-
-}
-
-resource "jenkins_job" "m-job" {
-  depends_on = [jenkins_folder.folders]
-
-  count  = length(var.m-jobs)
-  name   = lookup(element(var.m-jobs, count.index), "name", null)
-  folder = "/job/${lookup(element(var.m-jobs, count.index), "folder", null)}"
-
-  template = templatefile("${path.module}/mb-job.xml", {
-    repo_url = lookup(element(var.m-jobs, count.index), "repo_url", null)
-    name     = lookup(element(var.m-jobs, count.index), "name", null)
-  })
-
-  lifecycle {
-    ignore_changes = [template]
-  }
+  //  lifecycle {
+  //    ignore_changes = [template]
+  //  }
 
 }
 
 data "aws_instance" "jenkins" {
-  instance_id = "i-0c6467609af91bdea"
+  instance_id = "i-0ce70f83b8e7a2fe5"
 }
 
 resource "aws_route53_record" "jenkins" {
-  zone_id = "Z103214126L48SQW30RSR"
-  name    = "jenkins.devopsb71.online"
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "jenkins.learndevopseasy.online"
   type    = "A"
   ttl     = 30
   records = [data.aws_instance.jenkins.public_ip]
